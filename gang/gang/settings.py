c3259d734dd081226a9119494d0820c3735bc612
LaraTHiba <83555985+LaraTHiba@users.noreply.github.com>
2025-05-31 20:11:11 +0300

    ++test

--- gang/gang/settings.py
+++ gang/gang/settings.py
@@ -153,6 +153,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Ensure proper UTF-8 encoding
+DEFAULT_CHARSET = 'utf-8'
+FILE_CHARSET = 'utf-8'
+
 # Custom user model
 AUTH_USER_MODEL = 'core.User'
 
